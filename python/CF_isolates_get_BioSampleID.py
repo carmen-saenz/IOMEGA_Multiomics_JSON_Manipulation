bee4acf1965f0f3b30258586485db1a1e6a4de85
rzv923 <carmen.ausejo@sund.ku.dk>
2020-03-25 19:33:22 +0100

    CF_isolates unique genome_label

--- python/CF_isolates_get_BioSampleID.py
+++ python/CF_isolates_get_BioSampleID.py
@@ -7,6 +7,7 @@ import csv
 from pandas import read_excel
 from Bio import SeqIO
 import ftputil
+from ftplib import FTP
 
 # python3 CF_isolates_get_BioSampleID.py -i /Bacterial\ Cultures\ Catalogue.xlsx -o CF_isolates_BioSampleID.csv
 arguments = sys.argv
@@ -27,22 +28,23 @@ def get_old_id(old_id):
     """Get Assembly ID from Accession ID"""
     path = '/genomes/archive/old_refseq/Bacteria/%s' % (old_id)
     # Access to ftp NCBI with directory name for organism from RefSeq Ftp path
-    host.chdir(path)
-    # Get list of the directory with files for organism: GCF_<id for organism>_<Assembly ID>(_genomic.fna.gz, _assembly_report.txt,....)
-    dir_list = host.listdir(host.curdir)
-    nucl_id = dir_list[0].split('.')[0]
+    # Get first file name
+    file_name = host.nlst(path)[0].strip().split('/')[-1]
+    nucl_id = file_name.split('.')[0]
     return nucl_id
 
 def get_biosample_id(old_id):
     """Get nnew RefSeq ID from an old RefSeq ID"""
     from Bio import Entrez
     # provide your own mail here
-    Entrez.email =  # email
+    Entrez.email = # email
     nucl_id = get_old_id(old_id)
     handle = Entrez.efetch(db='nucleotide', id=nucl_id, rettype="gb", retmode="text")
     record = SeqIO.read(handle, "genbank")
     ref_id = record.dbxrefs
     biosample_id = str([x for x in ref_id if x.startswith('BioSample:')]).strip("['BioSample']").replace(":", "")
+    if biosample_id == '':
+        biosample_id = str([x for x in ref_id if x.startswith('BioProject')]).strip("['BioProject']").replace(":", "")
     return biosample_id
 
 # ---------------------------------------------------------------
@@ -50,18 +52,19 @@ def get_biosample_id(old_id):
 # ---------------------------------------------------------------
 sheet_df = read_excel(input_file, sheet_name = my_sheet)
 print(sheet_df.head()) # shows headers with top 5 rows
-old_refseq = sheet_df['Genome Name / Sample Name'].tolist()
-
+old_refseq = sheet_df['Genome Name / Sample Name'].drop_duplicates().tolist()
 # ---------------------------------------------------------------
 # Get GenBank ID - NCBI
 # ---------------------------------------------------------------
 biosample_dic = {}
 
 # Access to ftp NCBI
-host = ftputil.FTPHost('ftp.ncbi.nlm.nih.gov', 'anonymous', 'password')
+#host = ftputil.FTPHost('ftp.ncbi.nlm.nih.gov', 'anonymous', 'password')
+#host = FTP('ftp.ncbi.nlm.nih.gov', 'anonymous', 'password')
 
 for idx, old_id in enumerate(old_refseq):
     try:
+        host = FTP('ftp.ncbi.nlm.nih.gov', 'anonymous', 'password')
         biosample_id = get_biosample_id(old_id)
         biosample_dic[old_id] = biosample_id
     except:
@@ -71,6 +74,23 @@ for idx, old_id in enumerate(old_refseq):
     print(str(idx + 1) + "/" + str(len(old_refseq)))
     time.sleep(4)  # Delays for 4 seconds
 
+#for k in not_found:
+for k, v in biosample_dic.items():
+    if v == 'nan':
+        print('Not found: ', k)
+        try:
+            host = FTP('ftp.ncbi.nlm.nih.gov', 'anonymous', 'password')
+            biosample_id = get_biosample_id(k)
+            biosample_dic[k] = biosample_id
+        except:
+            print("Old Refseq ID not found: " + k)
+            biosample_dic[k] = ('-')
+    else:
+        #print(k)
+        biosample_dic[k] = v
+        continue
+    time.sleep(4)
+
 # Save GenBank IDs in a Df
 entrez_df = pd.DataFrame(biosample_dic.items(), columns=['Genome Name / Sample Name','Biosample Accession'])
 # Replace 'Biosample Accession' with updated IDs
@@ -79,5 +99,3 @@ sheet_df.update(entrez_df.set_index('Genome Name / Sample Name'))
 sheet_df.reset_index(inplace=True)
 sheet_df.to_csv(output_file, sep="\t", quoting=csv.QUOTE_NONE, index=False, header=True)
 print("Done!")
-
-
